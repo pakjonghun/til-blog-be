ef546337fc3afa95a00827f7c19f4e55cd7a6f5d
정훈박 <fireking5997@gmail.com>
2022-06-16 01:30:38 +0900

    fix:cors

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:8080", "http://localhost:3000"],
+    origin: [
+      "http://127.0.0.1:8080",
+      "http://127.0.0.1:3000",
+      "http://127.0.0.1:80",
+    ],
     methods: ["GET"],
   })
 );
